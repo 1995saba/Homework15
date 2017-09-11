6c9d82773294aaae4c02d2fbbe6f6d206d7ad84b
Sabina <sabina.orunbaeva@gmail.com>
2017-09-11 21:44:48 +0600

    Add files via upload

---
+++ Task1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Book> bookCollection = new List<Book>();
+            int size = 10;
+            
+            string path = @"C:\data";
+
+            for(int i=0; i < size; i++)
+            {
+                Book book = new Book();
+                book.Name = "asd";
+                book.Price = 100*i+0.5;
+                book.Author = "asd";
+                book.Year = 1990+i;
+
+                bookCollection.Add(book);
+            }
+
+            FileStream stream = new FileStream(path + @"\book.dat", FileMode.Create);
+            
+            BinaryFormatter formatter = new BinaryFormatter();
+            try
+            {
+                formatter.Serialize(stream, bookCollection);
+            }
+            catch(SerializationException ex)
+            {
+                Console.WriteLine("Failed to serialize. Reason: " + ex.Message);
+                throw;
+            }
+            finally
+            {
+                stream.Close();
+            }
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task1;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Book> bookCollection = new List<Book>();
+            string path = @"C:\data";
+            FileStream stream = new FileStream(path + @"\book.dat", FileMode.Open);
+
+            try
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                bookCollection = (List<Book>)formatter.Deserialize(stream);
+            }
+            catch(SerializationException ex)
+            {
+                Console.WriteLine("Failed to deserialize. Reason: " + ex.Message);
+                throw;
+            }
+
+            finally
+            {
+                stream.Close();
+            }
+
+            foreach(Book book in bookCollection)
+            {
+                Console.WriteLine("Name: {0}", book.Name);
+                Console.WriteLine("Price: {0} USD", book.Price);
+                Console.WriteLine("Author: {0}", book.Author);
+                Console.WriteLine("Year: {0}", book.Year);
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
